1bdd330b78cc7d0e4cac3af2eabef281df552257
Maxime <maxime.levrault@epfedu.fr>
2024-09-23 18:13:19 +0200

    version finale

---
+++ TP1_guessMyNumber_LEVRAULT/src/tp1_guessmynumber_levrault/TP1_guessMyNumber_LEVRAULT.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_guessmynumber_levrault;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Maxime
+ */
+public class TP1_guessMyNumber_LEVRAULT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random generateurAleat = new Random();
+
+        /*for (int i = 0; i < 5; i++) {
+            int n = generateurAleat.nextInt(100);
+            System.out.println(n);*/
+        
+        int nbr = generateurAleat.nextInt(100);
+        int choice = scanner.nextInt();
+        
+        }
+    }
+
+}
