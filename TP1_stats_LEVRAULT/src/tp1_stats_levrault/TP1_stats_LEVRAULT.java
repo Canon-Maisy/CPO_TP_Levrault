28df991a87217cb79639aea1b5bbc394914a94d1
Maxime <maxime.levrault@epfedu.fr>
2024-09-26 21:14:40 +0200

    version finale

---
+++ TP1_stats_LEVRAULT/src/tp1_stats_levrault/TP1_stats_LEVRAULT.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_stats_levrault;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Random;
+
+/**
+ *
+ * @author Maxime
+ */
+public class TP1_stats_LEVRAULT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int m;
+        Random random = new Random();
+        int[] tab = new int[6];
+        for (int i = 0; i < 6; i++) {
+            tab[i] = 0;
+        }
+        m = scanner.nextInt();
+        for (int i = 0; i < m; i++) {
+            int randomNumber = random.nextInt(6);
+            tab[randomNumber] += 1;
+        }
+        System.out.println(Arrays.toString(tab));
+        
+        for (int i = 0; i<6; i++){
+            double pourcentage = (tab[i]/(m*1.0))*100;
+            System.out.println(pourcentage);
+        }
+    }
+
+}
