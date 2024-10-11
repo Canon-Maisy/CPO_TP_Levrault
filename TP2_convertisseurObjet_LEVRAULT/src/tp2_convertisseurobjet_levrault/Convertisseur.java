e9896db7474b444152fed7e813b089be4cd94dfc
Maxime <maxime.levrault@epfedu.fr>
2024-10-11 11:15:22 +0200

    1

---
+++ TP2_convertisseurObjet_LEVRAULT/src/tp2_convertisseurobjet_levrault/Convertisseur.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_convertisseurobjet_levrault;
+
+/**
+ *
+ * @author Maxime
+ */
+public class Convertisseur {
+    int nbConversions;
+    
+    public Convertisseur(){
+        nbConversions = 0;
+    }
+    
+    public String toString(){
+        return "nb de conversions"+ nbConversions;
+    }
+}
