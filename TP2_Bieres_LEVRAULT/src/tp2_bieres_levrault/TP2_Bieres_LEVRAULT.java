16a16eeb40fad86e9980e95baf175bcc2d873848
Maxime <maxime.levrault@epfedu.fr>
2024-10-11 10:49:30 +0200

    1

--- TP2_Bieres_LEVRAULT/src/tp2_bieres_levrault/TP2_Bieres_LEVRAULT.java
+++ TP2_Bieres_LEVRAULT/src/tp2_bieres_levrault/TP2_Bieres_LEVRAULT.java
@@ -14,7 +14,24 @@ public class TP2_Bieres_LEVRAULT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        BouteilleBiere UneBiere = new BouteilleBiere("Cuvée des trolls", 7.0, "Dubuisson");
+        UneBiere.lireEtiquette();
+        
+        /*BouteilleBiere BiereLeffe = new  BouteilleBiere("Leffe", 6.6, "Abbaye de Leffe");
+        BiereLeffe.lireEtiquette();
+        
+        BouteilleBiere BiereChouffe = new BouteilleBiere("Chouffe", 8.0, "Achouffe");
+        BiereChouffe.lireEtiquette();
+        
+        BouteilleBiere BiereDespe = new BouteilleBiere ("Desperados", 5.9, "Heineken");
+        BiereDespe.lireEtiquette();
+        
+        BouteilleBiere BierePelforth = new BouteilleBiere("Pelforth", 5.8, "Brasserie du pélican");
+        BierePelforth.lireEtiquette();*/
+        
+        System.out.println(UneBiere.Décapsuler());
+        System.out.println(UneBiere.Décapsuler());
+        
     }
     
-}
+}   
