d3d97e0be9149da7d9fe5df9d7d7cbf06b4baee1
Maxime <maxime.levrault@epfedu.fr>
2024-10-16 08:36:27 +0200

    1

---
+++ nouvelle_interface_graphique/src/nouvelle_interface_graphique/Nouvelle_interface_graphique.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package nouvelle_interface_graphique;
+
+/**
+ *
+ * @author Maxime
+ */
+public class Nouvelle_interface_graphique {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        seconde_fenetre f = new seconde_fenetre();
+    }
+    
+}
