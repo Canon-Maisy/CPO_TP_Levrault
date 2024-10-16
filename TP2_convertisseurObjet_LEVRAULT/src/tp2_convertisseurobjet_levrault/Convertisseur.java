8aa699da84180dc60ff218774f5f372cf08e4392
Maxime <maxime.levrault@epfedu.fr>
2024-10-16 09:04:34 +0200

    création méthodes convertisseur

--- TP2_convertisseurObjet_LEVRAULT/src/tp2_convertisseurobjet_levrault/Convertisseur.java
+++ TP2_convertisseurObjet_LEVRAULT/src/tp2_convertisseurobjet_levrault/Convertisseur.java
@@ -9,13 +9,38 @@ package tp2_convertisseurobjet_levrault;
  * @author Maxime
  */
 public class Convertisseur {
+
     int nbConversions;
-    
-    public Convertisseur(){
+
+    public Convertisseur() {
         nbConversions = 0;
     }
+
+    public double CelciusVersKelvin(double tCelsuis) {
+        return tCelsuis + 273.25;
+    }
+
+    public double KelvinVersCelcius (double tKelvin) {
+        return tKelvin - 273.15;
+    }
     
-    public String toString(){
-        return "nb de conversions"+ nbConversions;
+    public double FarenheitVersCelcius(double tFarenheit) {
+        return (tFarenheit - 32) / 1.8;
+    }
+    
+    public double CelciusVersFarenheit(double tCelcius) {
+        return (tCelcius * 1.8) + 32;
+    }
+    
+    public double KelvinVersFarenheit(double tKelvin) {
+        return (tKelvin - 273.15) * 1.8 + 32;
+    }
+    
+    public double FarenheitVersKelvin(double tFarenheit) {
+        return (tFarenheit - 32) * 1.8 + 273.15;
+    }
+    
+    public String toString() {
+        return "nb de conversions" + nbConversions;
     }
 }
