513bf997a8dd6d8199a165e8466e18bb97bb1ed0
Maxime <maxime.levrault@epfedu.fr>
2024-10-16 09:19:55 +0200

    ajout incrémentation nbConversions

--- TP2_convertisseurObjet_LEVRAULT/src/tp2_convertisseurobjet_levrault/TP2_convertisseurObjet_LEVRAULT.java
+++ TP2_convertisseurObjet_LEVRAULT/src/tp2_convertisseurobjet_levrault/TP2_convertisseurObjet_LEVRAULT.java
@@ -14,7 +14,11 @@ public class TP2_convertisseurObjet_LEVRAULT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Convertisseur compteur = new Convertisseur ();
+        double temp_C = 10.0;
+        double temp_K = compteur.CelciusVersKelvin(temp_C);
+        System.out.println(compteur);
+        System.out.println("température finale : " + temp_K);
     }
     
 }
