d36a8e8c7a1577ba672d22836edfdb0d16acc191
Maxime <maxime.levrault@epfedu.fr>
2024-10-16 09:36:18 +0200

    ajout conversions dans code principal

--- TP2_convertisseurObjet_LEVRAULT/src/tp2_convertisseurobjet_levrault/TP2_convertisseurObjet_LEVRAULT.java
+++ TP2_convertisseurObjet_LEVRAULT/src/tp2_convertisseurobjet_levrault/TP2_convertisseurObjet_LEVRAULT.java
@@ -14,11 +14,26 @@ public class TP2_convertisseurObjet_LEVRAULT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Convertisseur compteur = new Convertisseur ();
+        
+        //conversion de Celsuis a Kelvins
+        Convertisseur convert_1 = new Convertisseur ();
         double temp_C = 10.0;
-        double temp_K = compteur.CelciusVersKelvin(temp_C);
-        System.out.println(compteur);
+        double temp_F = 0.0;
+        double temp_K = 0.0;
+        temp_K = convert_1.CelciusVersKelvin(temp_C);
+        System.out.println(convert_1);
         System.out.println("température finale : " + temp_K);
+        
+        
+        // conversions de Kelvin à Celsuis à Fareinheit puis de nouveau à Kelvins
+        Convertisseur convert_2 = new Convertisseur();
+        temp_K = 300.0;
+        temp_C = convert_2.KelvinVersCelcius(temp_K);
+        temp_F = convert_2.CelciusVersFarenheit(temp_C);
+        temp_F = convert_2.KelvinVersFarenheit(temp_K);
+        System.out.println(convert_2);
+        System.out.println("température finale : " + temp_F);
+        
     }
     
 }
