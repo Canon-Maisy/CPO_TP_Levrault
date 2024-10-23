a06d797b65a0f58c49e0db0754bff33112b00b22
Maxime <maxime.levrault@epfedu.fr>
2024-10-23 16:53:36 +0200

    ajout menu de selection pour l'utilisateur

--- TP2_convertisseurObjet_LEVRAULT/src/tp2_convertisseurobjet_levrault/TP2_convertisseurObjet_LEVRAULT.java
+++ TP2_convertisseurObjet_LEVRAULT/src/tp2_convertisseurobjet_levrault/TP2_convertisseurObjet_LEVRAULT.java
@@ -4,17 +4,20 @@
  */
 package tp2_convertisseurobjet_levrault;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Maxime
  */
 public class TP2_convertisseurObjet_LEVRAULT {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
+        /*
         //conversion de Celsuis a Kelvins
         Convertisseur convert_1 = new Convertisseur ();
         double temp_C = 10.0;
@@ -33,6 +36,49 @@ public class TP2_convertisseurObjet_LEVRAULT {
         temp_F = convert_2.KelvinVersFarenheit(temp_K);
         System.out.println(convert_2);
         System.out.println("température finale : " + temp_F);
+        */
+        
+        
+        
+        System.out.println("votre température :");
+        double temperature = scanner.nextInt();
+        System.out.println("en quelle unité est votre température ? (1 : °C, 2: K, 3: Fareinheit)");
+        int unité = scanner.nextInt();
+        System.out.println("en qeulle unité convertir ? (1 : °C, 2: K, 3: Fareinheit)");
+        int unité2 = scanner.nextInt();
+        Convertisseur convert_3 = new Convertisseur();
+        
+        if (unité == unité2){
+            System.out.println(temperature);
+        }else{ 
+
+            switch (unité){
+                case 1 :
+                    if (unité2 == 2){
+                        System.out.println(convert_3.CelciusVersKelvin(temperature));
+                        break;
+                    }else {
+                        System.out.println(convert_3.CelciusVersFarenheit(temperature));
+                        break;
+                    }
+                case 2 :
+                    if (unité2 == 1){
+                        System.out.println(convert_3.KelvinVersCelcius(temperature));
+                        break;
+                    } else {
+                        System.out.println(convert_3.KelvinVersFarenheit(temperature));
+                        break;
+                    }
+                case 3 :
+                    if (unité == 2){
+                        System.out.println(convert_3.FarenheitVersCelcius(temperature));
+                        break;
+                    }else {
+                        System.out.println(convert_3.FarenheitVersKelvin(temperature));
+                        break;
+                    }
+            }
+        }
         
     }
     
