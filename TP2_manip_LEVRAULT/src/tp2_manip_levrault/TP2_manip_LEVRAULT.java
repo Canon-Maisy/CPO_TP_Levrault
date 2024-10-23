d3eb4d9457e0455c958c880b45d1c73aed0f506f
Maxime <maxime.levrault@epfedu.fr>
2024-10-23 17:18:15 +0200

    création dernier exo TP2

---
+++ TP2_manip_LEVRAULT/src/tp2_manip_levrault/TP2_manip_LEVRAULT.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_manip_levrault;
+
+/**
+ *
+ * @author Maxime
+ */
+public class TP2_manip_LEVRAULT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Tartiflette assiette1 = new Tartiflette(500) ;
+        Tartiflette assiette2 = new Tartiflette(600) ;
+    }
+    
+}
