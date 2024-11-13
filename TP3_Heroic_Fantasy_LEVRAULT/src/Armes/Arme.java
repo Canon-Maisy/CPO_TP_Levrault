b5be2c8c5ad53e079d1825a89a041b2537f9c74e
Maxime <maxime.levrault@epfedu.fr>
2024-11-13 19:03:06 +0100

    tout

---
+++ TP3_Heroic_Fantasy_LEVRAULT/src/Armes/Arme.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Armes;
+
+/**
+ *
+ * @author Maxime
+ */
+public abstract class Arme {
+    private String nom;
+    private int attaque;
+    
+    //constructeur
+    public Arme (String nom, int attaque){
+        this.nom = nom;
+        
+        if (attaque > 100) {
+            this.attaque = 100; // On limite supérieurement à 100
+        } else if (attaque < 0){
+            this.attaque = 100; // On limite inférieurement à 0
+        } else {
+            this.attaque = attaque;
+        }
+    }
+    
+    //setter avec restriction
+    public void setAttaque(int attaque){
+        if (attaque > 100) {
+            this.attaque = 100; // On limite supérieurement à 100
+        } else if (attaque < 0){
+            this.attaque = 100; // On limite inférieurement à 0
+        } else {
+            this.attaque = attaque;}
+    }
+    
+    //getter pour l'attaque
+    public int getAttaque() {
+        return attaque;
+    }
+    //getter pour le nom
+    public String getNom(){
+        return nom;}
+
+    @Override
+    public String toString() {
+        return "Arme{" + "nom=" + nom + ", attaque=" + attaque + '}';
+    }
+    
+    
+}
