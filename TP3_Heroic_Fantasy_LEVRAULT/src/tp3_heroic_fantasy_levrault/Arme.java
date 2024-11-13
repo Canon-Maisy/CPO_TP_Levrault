cc8f94fbcd5a113c97c9ac1b63006423aae22ba1
Maxime <maxime.levrault@epfedu.fr>
2024-11-13 17:45:07 +0100

    fin création classe mere arme

--- TP3_Heroic_Fantasy_LEVRAULT/src/tp3_heroic_fantasy_levrault/Arme.java
+++ TP3_Heroic_Fantasy_LEVRAULT/src/tp3_heroic_fantasy_levrault/Arme.java
@@ -8,21 +8,25 @@ package tp3_heroic_fantasy_levrault;
  *
  * @author Maxime
  */
-public class Arme {
+public abstract class Arme {
     private String nom;
     private int attaque;
     
     //constructeur
     public Arme (String nom, int attaque){
         this.nom = nom;
-        this.attaque = attaque;
+        
+        if (attaque > 100) {
+            this.attaque = 100; // On limite à 100
+        } else {
+            this.attaque = attaque;
+        }
     }
     
     //setter avec restriction
     public void setAttaque(int attaque){
         if (attaque > 100) {
-            this.attaque = 0; // On limite à 100
-            System.out.println("valeur attaque impossible, fixé à 0");
+            this.attaque = 100; // On limite à 100
         } else {
             this.attaque = attaque;
         }
@@ -32,7 +36,7 @@ public class Arme {
     public int getAttaque() {
         return attaque;
     }
-    
+    //getter pour le nom
     public String getNom(){
         return nom;}
 
