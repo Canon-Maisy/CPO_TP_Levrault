32cf5ec0cd58046ed532faab451b1f494c365f42
Maxime <maxime.levrault@epfedu.fr>
2024-11-13 17:57:47 +0100

    création de la classe Epee

---
+++ TP3_Heroic_Fantasy_LEVRAULT/src/tp3_heroic_fantasy_levrault/Epee.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_heroic_fantasy_levrault;
+
+/**
+ *
+ * @author Maxime
+ */
+public class Epee extends Arme {
+    private int finesse;
+
+    public Epee(int finesse, String nom, int attaque) {
+        super(nom, attaque);
+        if (finesse > 100 ) {
+            this.finesse = 100;
+        }else if (finesse < 0){
+            this.finesse = 0;
+        }else{
+            this.finesse = finesse;}
+    }
+
+    public int getFinesse(){
+        return finesse;
+    }
+    
+    @Override
+    public String toString() {
+        return "Epee{" + "finesse=" + finesse + '}';
+    }
+    
+}
