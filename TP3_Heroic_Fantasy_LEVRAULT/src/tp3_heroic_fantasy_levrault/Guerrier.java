43250efe06df52bd1162524263d37faa31c1342d
Maxime <maxime.levrault@epfedu.fr>
2024-11-14 00:06:54 +0100

    création classe magicien

---
+++ TP3_Heroic_Fantasy_LEVRAULT/src/tp3_heroic_fantasy_levrault/Guerrier.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_heroic_fantasy_levrault;
+
+/**
+ *
+ * @author Maxime
+ */
+public class Guerrier extends Personnage {
+    private boolean cavalier;
+
+    //constructor
+    public Guerrier(boolean cavalier, String nom, int Pv) {
+        super(nom, Pv);
+        this.cavalier = cavalier;
+    }
+    
+    //getter
+    public boolean getCavalier(){
+        return cavalier;
+    }
+    
+    //setter
+    public void setCavalier(boolean cavalier){
+        this.cavalier = cavalier;
+    }
+    
+    //toString
+    @Override
+    public String toString() {
+        return "Guerrier{" + "cavalier=" + cavalier + '}';
+    }
+    
+}
