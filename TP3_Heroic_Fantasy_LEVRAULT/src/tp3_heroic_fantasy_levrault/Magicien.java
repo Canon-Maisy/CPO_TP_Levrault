35a4c1451f51bce9868f86e8af2f841bece5bc52
Maxime <maxime.levrault@epfedu.fr>
2024-11-13 23:52:27 +0100

    création classe magicien

---
+++ TP3_Heroic_Fantasy_LEVRAULT/src/tp3_heroic_fantasy_levrault/Magicien.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_heroic_fantasy_levrault;
+
+/**
+ *
+ * @author Maxime
+ */
+public class Magicien {
+
+    private boolean confirme;
+    
+    public Magicien(boolean confirme) {
+        this.confirme = confirme;
+    }
+
+    //getter
+    public boolean getConfirme() {
+        return confirme;
+    }
+    
+    //setter
+    public void setConfirme(boolean confirme) {
+        this.confirme = confirme;
+    }
+
+}
