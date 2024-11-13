97c6e68db64a4f62060657d2047f026afe902930
Maxime <maxime.levrault@epfedu.fr>
2024-11-14 00:10:59 +0100

    fin classe epee

--- TP3_Heroic_Fantasy_LEVRAULT/src/Armes/Epee.java
+++ TP3_Heroic_Fantasy_LEVRAULT/src/Armes/Epee.java
@@ -9,25 +9,27 @@ package Armes;
  * @author Maxime
  */
 public class Epee extends Arme {
+
     private int finesse;
 
     public Epee(int finesse, String nom, int attaque) {
         super(nom, attaque);
-        if (finesse > 100 ) {
+        if (finesse > 100) {
             this.finesse = 100;
-        }else if (finesse < 0){
+        } else if (finesse < 0) {
             this.finesse = 0;
-        }else{
-            this.finesse = finesse;}
+        } else {
+            this.finesse = finesse;
+        }
     }
 
-    public int getFinesse(){
+    public int getFinesse() {
         return finesse;
     }
-    
+
     @Override
     public String toString() {
         return "Epee{" + "finesse=" + finesse + '}';
     }
-    
+
 }
